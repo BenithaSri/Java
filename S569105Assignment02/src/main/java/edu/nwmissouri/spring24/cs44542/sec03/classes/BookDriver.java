5a15c42a72fccc7042c13c7111bc97cc02a29ba8
BenithaSri <156029071+BenithaSri@users.noreply.github.com>
2024-01-24 13:26:19 -0600

    Added javadoc

--- S569105Assignment02/src/main/java/edu/nwmissouri/spring24/cs44542/sec03/classes/BookDriver.java
+++ S569105Assignment02/src/main/java/edu/nwmissouri/spring24/cs44542/sec03/classes/BookDriver.java
@@ -5,8 +5,14 @@
 package edu.nwmissouri.spring24.cs44542.sec03.classes;
 import java.util.Scanner;
 /**
- *
- * @author S569105
+ *Class: 44542-03 Object-Oriented Programming
+ * 
+ * @author Benitha Sri Panchagiri
+ * Description: Making sure everything works
+ * Due: 01/25/2024
+ * I pledge that I have completed the programming assignment independently.
+ * I have not copied the code from a student, internet, or any other source.
+ * I will not share my source code with anyone under any circumstances.
  */
 
 public class BookDriver {
